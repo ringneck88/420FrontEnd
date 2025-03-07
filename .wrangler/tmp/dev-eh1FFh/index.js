d992c4944e6f9e07174773ccf3c7cc33f718397b
kelly-brightroot <ksharp@brightroot.com>
2025-03-07 01:43:11 -0700

    Update wrangler configuration for middleware integration and add dynamic middleware loader

--- .wrangler/tmp/dev-eh1FFh/index.js
+++ .wrangler/tmp/dev-eh1FFh/index.js
@@ -56,7 +56,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-oMFyTP/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-fq5IRs/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -88,7 +88,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-oMFyTP/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-fq5IRs/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
